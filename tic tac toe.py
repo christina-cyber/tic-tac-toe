0e023d9217135099f9ad3abdd7b8fa3c0caa6d30
Sameer Paudel <paudelsameer888@gmail.com>
2025-05-16 16:27:02 +0545

    Fixed issue of game not ending properly. O's chance not registering because of input not being typecasted to int

--- tic tac toe.py
+++ tic tac toe.py
@@ -1,4 +1,5 @@
 from unittest.util import three_way_cmp
+import os
 def sum(a,b,c):
     return a+b+c
 
@@ -25,9 +26,11 @@ def checkwin (xstate, zstate):
     wins=[[0,1,2], [3,4,5],[6,7,8],[0,4,8],[2,4,6],[0,3,6],[1,4,7],[2,5,8]]
     for win in wins:
         if (sum(xstate[win[0]], xstate[win[1]],xstate[win[2]])==3):
+            print(sum(xstate[win[0]], xstate[win[1]],xstate[win[2]]))
             print("x won the match")
             return 1
         if (sum(zstate[win[0]], zstate[win[1]],zstate[win[2]])==3):
+            print(sum(zstate[win[0]], zstate[win[1]],zstate[win[2]]))
             print("O won the match")
             return 0
     return -1
@@ -39,8 +42,10 @@ if __name__ == "__main__" :
     xstate =[0,0,0,0,0,0,0,0,0]
     zstate =[0,0,0,0,0,0,0,0,0]
     turn=1
+    gameover = False
     print("welcome to tic tac toe")
-    while(True):
+    while(not gameover):
+        os.system("clear")
         printBoard(xstate, zstate)
         if(turn==1):
             print("X's chance")
@@ -48,10 +53,12 @@ if __name__ == "__main__" :
             xstate[value] = 1
         else:
             print("O's chance")
-            value=(input("please enter a value: "))
+            value=int(input("please enter a value: "))
             zstate[value] = 1
         cwin = checkwin(xstate, zstate)
         if(cwin != -1):
             print("match over")
+            gameover = True
+            printBoard(xstate, zstate)
         turn= 1-turn
     
